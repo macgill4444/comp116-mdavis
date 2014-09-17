0ce408070740b9d83f63ee5eb563183f6f0f6524
root <root@localhost.localdomain>
2014-09-17 08:41:23 +0000

    almost done, just need to scan out payload

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -14,18 +14,39 @@ RST = 4
 SYN = 2
 FIN = 1
 
+$s404s = 0
+$nmaps = 0
+$shells = 0
 
 def web_log(_log)
 	infile = File.open(_log, "r")
 	while (word = infile.gets)
-		puts word
+		if word =~ /4[0-9][0-9]/
+			#parse_line(word, "HTTP 404 Error found")	
+			$s404s += 1
+		end
+		if word =~ /[n|N][m|M][a|A][p|P]/ 
+			#parse_line(word, "nmap found")
+			$nmaps += 1
+		end
+		if word =~ /x[0-9|a-f|A-F][0-9|a-f|A-F]{3,}/
+		#if word =~ /(%21)?(%2F)?bin%2F?(sh)?(%2F)?(bash)?/
+			$shells += 1
+			parse_line(word, "Shellcode found")
+		end	
+		
 	end
 end
 
-
-
-
-
+def parse_line(word, alert_ty)
+	
+	#puts word
+	#puts "************************************************************"
+	$src_ip = word.scan(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)
+	$payloadz = word.scan(/(["'])(?:(?=(\\?))\2.)*?\1/)
+	$incidence_number += 1
+	print_alert(alert_ty,"HTTP")
+end
 
 
  
@@ -67,27 +88,26 @@ def live_stream
 end
 
 #NEED TO DEFINE PROTOCOL
-def print_alert(attack)
+def print_alert(attack, protoc)
 	puts "\n"
-	puts "#{$incidence_number}. ALERT #{attack} is detected from #{$src_ip} (HTTP) (#{$payloadz})!" 
+	puts "#{$incidence_number}. ALERT #{attack} is detected from #{$src_ip} (#{protoc}) (#{$payloadz})!" 
 	puts "\n"
+	#clear vars
+	$payloadz = ""
+	$src_ip = -1
 end
 
-
 def ccard_check 
-	if /6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/.match($payloadz)
+	if $payloadz =~ /6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/
 		$incidence_number += 1
-		print_alert("Discoverty Credit Card")
-	elsif /3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/.match($payloadz)
+		print_alert("Discoverty Credit Card", "HTTP")
+	elsif $payloadz =~ /3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/
 		$incidence_number += 1
-		print_alert("Amex Credit Card")
-	elsif /5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/.match($payloadz)
+		print_alert("Amex Credit Card", "HTTP")
+	elsif $payload =~ /5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/
 		$incidence_number += 1
-		print_alert("Master Credit Card")	
+		print_alert("Master Credit Card", "HTTP")	
 	end
-
-	#clear payloadz
-	$payloadz = "empty payload"
 end
 
 def main 
@@ -96,6 +116,10 @@ def main
 	else
 		live_stream
 	end
+	
+	puts "404 errors #{$s404s}"
+	puts "nmaps #{$nmaps}"
+	puts "shells #{$shells}"
 end
 
 main
