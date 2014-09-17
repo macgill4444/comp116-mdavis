a43bd71a5fbaada97b4f605b708bfd24da6ccac9
root <root@localhost.localdomain>
2014-09-17 06:16:16 +0000

    started web log part of assignment

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,4 +1,4 @@
-#notes: can there be xmas and null with anything but a tcp? How do we do credit car example? Are we suppose to be able to parse through a file?
+#notes: http protocol? Is it always http? How do we figure out the others? ?
 
 require 'packetfu'
 
@@ -7,12 +7,31 @@ $ip_protocol = -1
 $src_ip = -1
 $payloadz = -1
 
+URG = 32
+ACK = 16
+PSH = 8
+RST = 4
+SYN = 2
+FIN = 1
+
+
+def web_log(_log)
+	infile = File.open(_log, "r")
+	while (word = infile.gets)
+		puts word
+	end
+end
+
+
+
+
+
+
+
  
 def live_stream
 	packets = PacketFu::Capture.new(:start => true, :iface =>'eth0', :promisc => true, :filter => "tcp")
 		
-	puts "starting"
-	
 	caught = false
 	while caught == false do 
 		packets.stream.each do |p|
@@ -24,29 +43,20 @@ def live_stream
 			pkt.tcp_flags.each do |f| 
 				flags_sum += f
 			end
+			
 			#collect information 
 			$src_ip = pkt.ip_src_readable
 			$ip_protocol = pkt.ip_proto
 			$payloadz = pkt.payload() 
-			#puts "Source IP is  #{$src_ip}"
-			#puts "IP protocol is #{$ip_protocol}"
-			#puts "PAYLOAD IS         #{$payloadz}"	
 					
-			#destinatino port for TCP, 80 is http
-			#puts pkt.tcp_dst
-			#puts "Destinatino port is above me..."
-				
-			#if flag sum is 6, all flags on and xmas	
-			if flags_sum == 6
-				print_alert("Xmas attack")
+			if flags_sum == (URG + PSH + SYN)
 				$incidence_number += 1			
-			#if flag sum is 0 no flags on so null	
+				print_alert("Xmas attack")
 			elsif flags_sum == 0
 				$incidence_number += 1
-				print_alert("Null attack ")
+				print_alert("Null attack")
 			else 
 				if pkt.tcp_dst == 80 
-					#puts "is a http req"
 					ccard_check		
 				end		
 			end
@@ -56,14 +66,12 @@ def live_stream
 
 end
 
-#{incident_number}. ALERT: #{attack} is detected from #{source IP address} (#{protocol}) (#{payload})!
-
-
+#NEED TO DEFINE PROTOCOL
 def print_alert(attack)
-	puts "#{$incidence_number}. ALERT #{attack} is detected from #{$src_ip} (#{$ip_protocol}) (#{$payloadz}" 
+	puts "\n"
+	puts "#{$incidence_number}. ALERT #{attack} is detected from #{$src_ip} (HTTP) (#{$payloadz})!" 
+	puts "\n"
 end
-
-
 
 
 def ccard_check 
@@ -72,18 +80,24 @@ def ccard_check
 		print_alert("Discoverty Credit Card")
 	elsif /3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/.match($payloadz)
 		$incidence_number += 1
-		puts "Amex detected"
+		print_alert("Amex Credit Card")
 	elsif /5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/.match($payloadz)
 		$incidence_number += 1
-		puts "Mastercard detected"
+		print_alert("Master Credit Card")	
 	end
+
 	#clear payloadz
 	$payloadz = "empty payload"
 end
 
+def main 
+	if ARGV[0] == '-r'
+		web_log(ARGV[1])	
+	else
+		live_stream
+	end
+end
 
-live_stream
-
-
+main
 
 
