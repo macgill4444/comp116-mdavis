5e3ef8d46d39ac8b5c11caf32d89a8594459375c
root <root@localhost.localdomain>
2014-09-17 00:13:08 +0000

    getting the tcp flags

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,13 +1,21 @@
 require 'packetfu'
 
-packets = PacketFu::Capture.new(:start => true, :iface =>'eth0', :promisc => true)
+packets = PacketFu::Capture.new(:start => true, :iface =>'eth0', :promisc => true, :filter => "tcp and udp")
 
 caught = false
 while caught == false do 
 	packets.stream.each do |p|
 		pkt  = PacketFu::Packet.parse(p)
-		puts pkt
+		puts pkt.inspect()		
+		puts "*********************************"
+		pkt.tcp_flags.each do |f| 
+			puts f
+		end
+		puts "++++++++++++++++++++++++++++++++++++++++"
 	end
+	
+	
+
 end
 
 
